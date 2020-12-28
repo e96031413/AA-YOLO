2beb9d2a088a348e52205389f1115f471ed6c939
e96031413 <30921855+e96031413@users.noreply.github.com>
2020-12-28 09:44:55 +0800

    Update layers.py

--- utils/layers.py
+++ utils/layers.py
@@ -158,3 +158,18 @@ class Swish(nn.Module):
 class HardSwish(nn.Module):  # https://arxiv.org/pdf/1905.02244.pdf
     def forward(self, x):
         return x * F.hardtanh(x + 3, 0., 6., True) / 6.
+
+
+class FReLU(nn.Module):
+    r""" FReLU formulation. The funnel condition has a window size of kxk. (k=3 by default)
+    """
+    def __init__(self, in_channels):
+        super().__init__()
+        self.conv_frelu = nn.Conv2d(in_channels, in_channels, 3, 1, 1, groups=in_channels)
+        self.bn_frelu = nn.BatchNorm2d(in_channels)
+
+    def forward(self, x):
+        x1 = self.conv_frelu(x)
+        x1 = self.bn_frelu(x1)
+        x = torch.maximum(x, x1)
+        return x
