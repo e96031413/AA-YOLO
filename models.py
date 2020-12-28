edd75509fcf5131cdac5e670b667aff9bc757e76
e96031413 <30921855+e96031413@users.noreply.github.com>
2020-12-28 09:44:27 +0800

    Update models.py

--- models.py
+++ models.py
@@ -582,17 +582,3 @@ class ECALayer(nn.Module):
         # Multi-scale information fusion
         y = self.sigmoid(y)
         return x * y.expand_as(x)
-
-class FReLU(M.Module):
-    r""" FReLU formulation. The funnel condition has a window size of kxk. (k=3 by default)
-    """
-    def __init__(self, in_channels):
-        super().__init__()
-        self.conv_frelu = nn.Conv2d(in_channels, in_channels, 3, 1, 1, groups=in_channels)
-        self.bn_frelu = nn.BatchNorm2d(in_channels)
-
-    def forward(self, x):
-        x1 = self.conv_frelu(x)
-        x1 = self.bn_frelu(x1)
-        x = torch.maximum(x, x1)
-        return x
